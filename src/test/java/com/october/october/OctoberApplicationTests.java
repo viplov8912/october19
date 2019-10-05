2278759426b4851065964bd7c02050ea44633d2e
viplov8912 <viplov8912@gmail.com>
2019-10-06 03:19:02 +0530

    first commit of october

---
+++ src/test/java/com/october/october/OctoberApplicationTests.java
@@ -0,0 +1,16 @@
+package com.october.october;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class OctoberApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
